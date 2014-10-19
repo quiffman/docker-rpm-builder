35b356fad96d59bc1f63ff59a2d520851efe15a6
Richard Guest <r.guest@gns.cri.nz>
2014-10-20 11:33:20 +1300

    Use abspath function so this will work on OSX too.

--- docker-build-binary-rpm-from-dir.sh
+++ docker-build-binary-rpm-from-dir.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+function abspath() { pushd . > /dev/null; if [ -d "$1" ]; then cd "$1"; dirs -l +0; else cd "`dirname \"$1\"`"; cur_dir=`dirs -l +0`; if [ "$cur_dir" == "/" ]; then echo "$cur_dir`basename \"$1\"`"; else echo "$cur_dir/`basename \"$1\"`"; fi; fi; popd > /dev/null; }
+
 set -e
 USAGE="Usage:\n$0 IMAGETAG SRCDIR <...additional docker options..>\n"
 IMAGETAG=$1
@@ -7,9 +10,9 @@ shift 1
 SRCDIR=$1
 [[ -n "$1" && -d $1 && -r $1 && -x $1 && -w $1 ]] || { echo "ERROR: Missing SRCDIR or wrong permissions - must be an rwx directory" ; echo -e $USAGE ; exit 1 ;}
 shift 1
-SRCDIR=$(readlink -f ${SRCDIR})
+SRCDIR=$(abspath ${SRCDIR})
 echo "Now building project from $SRCDIR on image $IMAGETAG"
 
-CURRENTDIR=$(dirname $(readlink -f $0))
+CURRENTDIR=$(dirname $(abspath $0))
 
 docker run $* -v ${CURRENTDIR}/docker-scripts:/docker-scripts -v ${SRCDIR}:/src -w /docker-scripts ${IMAGETAG} ./rpmbuild-in-docker.sh $(id -u):$(id -g)
